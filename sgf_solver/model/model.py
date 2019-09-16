4593f9bddb34a5d23aeedf1528456b7a401f81d9
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2019-09-16 22:05:10 +0300

    namings

--- sgf_solver/model/model.py
+++ sgf_solver/model/model.py
@@ -6,48 +6,48 @@ from keras.regularizers import l2
 
 INPUT_DATA_SHAPE = (1, 19, 19)
 
-RESIDUAL_LAYERS = 10
-FILTER_SIZE = 32
+CHANNELS_AMOUNT = 32
+RESIDUAL_BLOCKS = 8
 
 L2_CONST = 1e-4
 
-RegularizedConv2D = partial(Conv2D, kernel_regularizer=l2(1e-4))
-PaddedConv2D = partial(Conv2D, padding='same', kernel_regularizer=l2(1e-4))
+RegularizedConv2D = partial(Conv2D, kernel_regularizer=l2(L2_CONST))
+PaddedConv2D = partial(Conv2D, padding='same', kernel_regularizer=l2(L2_CONST))
 
 
 def create_model():
     input_ = Input(shape=INPUT_DATA_SHAPE)
 
     layer = input_
-    layer = RegularizedConv2D(FILTER_SIZE, (1, 1))(layer)
+    layer = RegularizedConv2D(CHANNELS_AMOUNT, (1, 1))(layer)
     layer = BatchNormalization()(layer)
     layer = Activation('relu')(layer)
 
-    layer = PaddedConv2D(FILTER_SIZE, (3, 3))(layer)
+    layer = PaddedConv2D(CHANNELS_AMOUNT, (3, 3))(layer)
     layer = BatchNormalization()(layer)
     layer = Activation('relu')(layer)
 
-    for _ in range(RESIDUAL_LAYERS):
+    for _ in range(RESIDUAL_BLOCKS):
         res = layer
-        layer = PaddedConv2D(FILTER_SIZE, (3, 3))(layer)
+        layer = PaddedConv2D(CHANNELS_AMOUNT, (3, 3))(layer)
         layer = BatchNormalization()(layer)
         layer = Activation('relu')(layer)
 
-        layer = PaddedConv2D(FILTER_SIZE, (3, 3))(layer)
+        layer = PaddedConv2D(CHANNELS_AMOUNT, (3, 3))(layer)
         layer = BatchNormalization()(layer)
 
         layer = Add()([layer, res])
         layer = Activation('relu')(layer)
 
     value_head = layer
-    value_head = Dense(FILTER_SIZE, kernel_regularizer=l2(1e-4))(value_head)
+    value_head = Dense(CHANNELS_AMOUNT, kernel_regularizer=l2(1e-4))(value_head)
     value_head = Activation("relu")(value_head)
     value_head = Flatten()(value_head)
     value_head = Dense(1)(value_head)
     value_head = Activation("tanh", name="vh")(value_head)
 
     policy_head = layer
-    policy_head = RegularizedConv2D(FILTER_SIZE, (1, 1))(policy_head)
+    policy_head = RegularizedConv2D(CHANNELS_AMOUNT, (1, 1))(policy_head)
     policy_head = BatchNormalization()(policy_head)
     policy_head = Activation('relu')(policy_head)
     policy_head = Flatten()(policy_head)
